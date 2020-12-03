f9043d9affa3b1f2b36cf4e03a4a0fea3520e5b5
Jason Gray <70484637+JasonGray55@users.noreply.github.com>
2020-12-03 12:41:24 -0500

    Update MessageServer.java

--- Project5 5/src/MessageServer.java
+++ Project5 5/src/MessageServer.java
@@ -1,54 +1,16 @@
-package main;
-
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-
-public class MessageServer {
-    public static ArrayList<User> users = new ArrayList<>();
-
-
-    public ArrayList<User> getUsers() {
-        return users;
-    }
-
+public class MessageServer extends Thread{
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(4242);
-        System.out.println("Waiting for the client to connect...");
         Socket socket = serverSocket.accept();
-        System.out.println("Client connected!");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter writer = new PrintWriter(socket.getOutputStream());
-        while (true) {
-            String equations = reader.readLine();
-            if (equations.equals("End")) {
-                serverSocket.close();
-                break;
-            }
-            String[] partsOfEquation = equations.split(" ");
-            double one = Double.parseDouble(partsOfEquation[0]);
-            double two = Double.parseDouble(partsOfEquation[2]);
-            String operand = partsOfEquation[1];
-            String allTogether = doCalculation(one, two, operand);
-            writer.write(allTogether);
-            writer.println();
-            writer.flush();
-        }
-    }
-    public static String loadUsers(){
-        String fileName= "User.txt";
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            String line;
-            while ((line = br.readLine())!=null){
-                String[] user= line.split(", ");
-                User newUser = new User(user[0], user[1], user[2], Integer.parseInt(user[3]), user[3], user[4]);
-                users.add(newUser);
-            }
-        }  catch (IOException e) {
-            e.printStackTrace();
-        }
+        // HERE USER HAS CONNECTED SO WE WILL NOW SEND THEM THE LOGIN AND CREATE ACCOUNT GUIS.
+        // VINNY HAS LOGIN AND CREATE ACCOUNT GUIS.
+        // I THINK THAT THREADS ARE CREATED AFTER USER PRESSES LOGIN.
+
+
     }
 }
